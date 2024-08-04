75874eac54b695f700d64bcb34361f1d6b87dd46
yashh-thakur <yashhh.thakurrr@gmail.com>
2024-08-04 19:27:22 +0530

    commits added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import Layout from './NavigationBar/Sidebar';
+import Home from './Home/Home';
+
+function App() {
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
+}
+
+export default App;
